837685cbb35fe859a91edc5df0b525b8d591e349
gurujeya14 <glowguru14@gmail.com>
2017-05-11 19:29:56 +0530

    Update Init_dbAndTableCreation.py

--- Init_dbAndTableCreation.py
+++ Init_dbAndTableCreation.py
@@ -6,5 +6,6 @@ print "Opened database successfully";
 
 #Create Required Tables
 conn.execute('CREATE TABLE users (id TEXT NOT NULL UNIQUE PRIMARY KEY, uname TEXT, paswd TEXT)')
-print "Table created successfully";
+conn.execute('CREATE TABLE bugs (pdtNm TEXT, pdtVer TEXT, bug TEXT NOT NULL UNIQUE PRIMARY KEY, des TEXT, severe TEXT, status TEXT, date TEXT, chgUser TEXT)')
+print "Tables created successfully";
 conn.close()
